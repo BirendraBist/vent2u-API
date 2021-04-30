de4fed5728d21363470fb2d9a332d562ea288af2
birendra <bistbirendra2017@gmail.com>
2021-04-30 16:26:58 +0200

    update usermodel

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,17 +1,24 @@
-// module.exports = (sequelize, Sequelize) => {
-//     const User = sequelize.define("user", {
-//         id: {
-//             primaryKey: true,
-//             autoIncrement: true,
-//             type: Sequelize.INTEGER
-//         },
-//         username: {
-//             type: Sequelize.STRING
-//         },
-//         password: {
-//             type: Sequelize.STRING
-//         },
-//     });
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: Sequelize.INTEGER,
+    },
+    room_id: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    username: {
+      type: Sequelize.STRING,
+    },
+    // password: {
+    //     type: Sequelize.STRING
+    // },
+  });
 
-//     return User;
-// };
+  return User;
+};
